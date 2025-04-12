a419a2cdd31b68ff954d5413e6dacfe995e0cb62
kuman514 <hadjadj0@gmail.com>
2025-04-13 06:34:42 +0900

    feat: add list item dragging using html drag api

--- index.js
+++ index.js
@@ -1,8 +1,58 @@
 (() => {
   document.addEventListener('DOMContentLoaded', () => {
-    let isClicked = false;
+    let newItemId = 0;
+    const items = [];
+
+    let draggedItem = null;
 
     const draggableList = document.querySelector('#draggable-list');
+
+    function generateListItem(newItem) {
+      const newListItemContent = document.createElement('span');
+      newListItemContent.textContent = newItem.value;
+      const newListItem = document.createElement('li');
+      newListItem.appendChild(newListItemContent);
+      newListItem.draggable = true;
+      newListItem.id = newItem.id;
+      draggableList.appendChild(newListItem);
+    }
+
+    draggableList.addEventListener('dragstart', (event) => {
+      draggedItem = event.target;
+    });
+    draggableList.addEventListener('dragover', (event) => {
+      event.preventDefault();
+    });
+    draggableList.addEventListener('drop', (event) => {
+      event.preventDefault();
+
+      const draggedItemIndex = items.findIndex(
+        (item) => draggedItem.id === item.id
+      );
+      const primaryTargetIndex = items.findIndex(
+        (item) => event.target.id === item.id
+      );
+
+      if (
+        draggedItemIndex === -1 ||
+        primaryTargetIndex === -1 ||
+        primaryTargetIndex === draggedItemIndex
+      ) {
+        return;
+      }
+
+      items.splice(primaryTargetIndex, 0, items[draggedItemIndex]);
+      if (primaryTargetIndex > draggedItemIndex) {
+        items.splice(draggedItemIndex, 1);
+      } else {
+        items.splice(draggedItemIndex + 1, 1);
+      }
+
+      draggableList.textContent = '';
+      items.forEach(generateListItem);
+
+      draggedItem = null;
+    });
 
     const itemForm = document.querySelector('#item-form');
     const itemFormText = document.querySelector('#item-form-text');
@@ -10,11 +60,16 @@
     itemForm.addEventListener('submit', (event) => {
       event.preventDefault();
 
-      const newListItem = document.createElement('li');
-      newListItem.textContent = itemFormText.value;
-      draggableList.appendChild(newListItem);
+      const newItem = {
+        id: String(newItemId),
+        value: itemFormText.value,
+      };
+      items.push(newItem);
+      generateListItem(newItem);
 
       itemFormText.value = '';
+      newItemId++;
+
       return false;
     });
   });
